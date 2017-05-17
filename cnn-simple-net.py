d9e528fb6192a8de2d94289bb7d790f69bf09ae7
phamquangkhang <phamquangkhang@gmail.com>
2017-05-18 01:28:06 +0900

    complete cnn net and create train

--- cnn-simple-net.py
+++ cnn-simple-net.py
@@ -41,4 +41,83 @@ class SimpleCnn:
 		self.params['b2'] = np.zeros(output_size)
 
 		#generate layers
+		self.layers = OrderedDict()
+		self.layers['Conv1'] = Convolution(self.params['W1'], self.params['b1'],
+											conv_param['stride'], conv_param['pad'])
+		self.layers['Relu1'] = Relu()
+		self.layers['Pool1'] = Pooling(pool_h=2, pool_w=2, stride=2)
+		self.layers['Affine1'] = Affine(self.params['W2'], self.params['b2'])
+		self.layers['Relu2'] = Relu()
+		self.layers['Affine2'] = Affine(self.params['W3'], self.params['b3'])
+
+		self.last_layer = SoftmaxWithLoss()
+
+	def predict(self, x):
+		for layer in self.layers.values():
+			x = layer.forward(x)
+		return x
+	def loss(self, x, t):
+		"""Calculate loss
+			Params: input data x, label t
+		"""
+		y = self.predict(x)
+		return self.last_layer.forward(y, t)
+	def accuracy(self, x, t, batch_size=100):
+		if t.ndim != 1 : t = np.argmax(t, axis=1)
+
+		acc = 0.0
+		for i in range(int(x.shape[0] / batch_size)):
+			tx = x[i*batch_size:(i+1)*batch_size]
+			tt = t[i*batch_size:(i+1)*batch_size]
+			y = self.predict(tx)
+			y = np.argmax(y, axis=1)
+			acc += np.sum(y == tt)
+		return acc / x.shape[0]
+	def gradient(self, x, t):
+		"""Calculate gradient
+
+		Params:
+		---------------------
+		x: input data
+		t: label
+
+		Returns:
+		---------------------
+		A dictionary contains the gradient of each layer
+		"""
+
+		#forward
+		self.loss(x,t)
+
+		#backward
+		dout = 1
+		dout = self.last_layer.backward(dout)
+
+		layers = list(self.layers.values())
+		layers.reverse()
+		for layer in layers:
+			dout = layer.backward(dout)
+
+		#setup
+		grads = {}
+		grads['W1'], grads['b1'] = self.layers['Conv1'].dW, self.layers['Conv1'].db
+        grads['W2'], grads['b2'] = self.layers['Affine1'].dW, self.layers['Affine1'].db
+        grads['W3'], grads['b3'] = self.layers['Affine2'].dW, self.layers['Affine2'].db
+
+        return grads
+
+    def save_params(self, file_name="params.pkl"):
+    	params = {}
+    	for key, val in self.params.items():
+    		params[key] = val
+    	with open(file_name, 'wb') as f:
+    		pickle.dump(params, f)
+    def load_params(self, file_name="params.pkl"):
+    	with open(file_name, 'rb') as f:
+    		params = pickle.load(f)
+    	with key, val in params.items():
+    		self.params[key] = val
+		for i, key enumerate(['Conv1', 'Affine1', 'Affine2']):
+			self.layers[key].W = self.params['W' + str(i+1)]
+			self.layers[key].b = self.params['b' + str(i+1)]
 		
---
+++ train-simple-cnn.py
@@ -0,0 +1,9 @@
+# coding:  utf-8
+import sys, os
+sys.path.append(os.pardir)
+import numpy as np
+import matplotlib.pyplot as plt
+import mnist_load
+from cnn-simple-net import SimpleCnn
+from train import train
+
